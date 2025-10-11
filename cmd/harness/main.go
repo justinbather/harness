a60fef52f41de32a2ef3bae3d4e5c9a88c57bd37
justinbather <justinbather@hotmail.com>
2025-10-11 18:07:41 +0100

    simple topic list tui

--- cmd/harness/main.go
+++ cmd/harness/main.go
@@ -2,16 +2,84 @@ package main
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"time"
+	"fmt"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/justinbather/harness/internal/harness"
 	"github.com/justinbather/harness/internal/logger"
 	"go.uber.org/zap"
 )
 
+type model struct {
+	topics    map[string]int
+	topicList []string
+	cursor    int
+	selected  map[int]struct{}
+}
+
+func initialModel(harness *harness.Harness) model {
+	var topicList []string
+	topicMp := harness.ListTopics()
+
+	for topic, _ := range topicMp {
+		topicList = append(topicList, topic)
+	}
+
+	return model{
+		topics:    topicMp,
+		topicList: topicList,
+		cursor:    0,
+		selected:  make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// key press
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.topicList)-1 {
+				m.cursor++
+			}
+		}
+
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Kafka Topics\n\n"
+	for i, topic := range m.topicList {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		s += fmt.Sprintf("%s %s %d\n", cursor, topic, m.topics[topic])
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
 func main() {
+
 	ctx := context.Background()
 	log, ctx := logger.FromCtx(ctx)
 
@@ -20,35 +88,12 @@ func main() {
 		panic(err)
 	}
 
-	sigs := make(chan os.Signal, 2)
-	signal.Notify(sigs, os.Interrupt)
-
-	go pollDB(harness)
-
 	harness.Start(ctx)
+	defer harness.Shutdown(ctx)
 
-	<-sigs
-	log.Info("got shutdown signal")
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		harness.Shutdown(ctx)
-	}()
-
-	select {
-	case <-sigs:
-		log.Info("got second interrupt; forcing shutdown")
-	case <-done:
+	p := tea.NewProgram(initialModel(harness))
+	if _, err := p.Run(); err != nil {
+		log.Error("running TUI", zap.Error(err))
 	}
-}
 
-func pollDB(harness *harness.Harness) {
-	log, _ := logger.FromCtx(context.Background())
-	for {
-		time.Sleep(2 * time.Second)
-		topics := harness.ListTopics()
-		for t, size := range topics {
-			log.Info("topic", zap.String("topic", t), zap.Int("size", size))
-		}
-	}
 }
